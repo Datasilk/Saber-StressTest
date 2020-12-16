505c51fc21636dad286be4e6b094446d71320082
Mark Entingh <entingh@gmail.com>
2020-12-15 22:36:42 -0800

    added info class

---
+++ Info.cs
@@ -0,0 +1,11 @@
+﻿using Saber.Vendor;
+namespace Saber.Vendors.StressTest
+{
+    public class Info : IVendorInfo
+    {
+        public string Key { get; set; } = "StressTest";
+        public string Name { get; set; } = "Stress Test";
+        public string Description { get; set; } = "Allows administrators to stress test their web server by loading 24 iframes that rapidly load random test pages from their website.";
+        public Version Version { get; set; } = "1.0.0.0";
+    }
+}
