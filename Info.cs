734e9fde09511f8874282b5d6a2c9cdd5e224f69
Mark Entingh <entingh@gmail.com>
2023-01-05 00:28:07 -0600

    fixed ambiguious name

--- Info.cs
+++ Info.cs
@@ -7,6 +7,6 @@ namespace Saber.Vendors.StressTest
         public string Name { get; set; } = "Stress Test";
         public string Description { get; set; } = "Allows administrators to stress test their web server by loading 24 iframes that rapidly load random test pages from their website.";
         public string Icon { get; set; }
-        public Version Version { get; set; } = "1.0.0.0";
+        public Vendor.Version Version { get; set; } = "1.0.0.0";
     }
 }
